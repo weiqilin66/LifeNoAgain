9ef99d1be3e16f5e45d5e0a70e4f8f53ce593c78
weiqilin66 <weiqilin66@gmail.com>
2020-09-22 08:41:25 +0800

    l

--- 前台/vuehr/src/views/sys/SysInit.vue
+++ 前台/vuehr/src/views/sys/SysInit.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>init</div>
+    <div>
+
+        <el-button @click="refresh">缓存刷新</el-button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "SysInit"
+        name: "SysInit",
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            refresh(){
+                this.getRequest("/noRight/cache/refresh").then(resp=>{
+                    if (resp) {
+                        this.$message.success("刷新成功");
+                    }
+                })
+            }
+        }
     }
 </script>
 
